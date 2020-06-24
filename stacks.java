55cbe190da8e76b9228b9edcfe08940ae646a3c3
Anya <annagapyuk@gmail.com>
2020-06-24 17:24:03 -0400

    created stack with an initial element and size method

--- stacks.java
+++ stacks.java
@@ -4,3 +4,13 @@ public class Stack <T> implements Iterable <T> {
     // create an empty stack
     public Stack() { }
 }
+
+// Create a Stack with an initial element
+public Stack (T firstElem) {
+    push(firstElem);
+}
+
+// return the number of elements in the stack
+public int size() {
+    return list.size();
+}
