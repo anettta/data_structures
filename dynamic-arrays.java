0f09b08d17cc0c916ec1737dac998dbc79d3cd1e
Anya <annagapyuk@gmail.com>
2020-06-24 12:16:45 -0400

    added iterator and toString methods

--- dynamic-arrays.java
+++ dynamic-arrays.java
@@ -70,3 +70,23 @@ public int indexOf(Object obj) {
 public boolean contains(Object obj) {
     return indexOf(obj) != -1;
 }
+
+// iterator is still fast but not as fast as iterative for loop
+@Override public java.util.Iterator <T> iterator () {
+    return new java.util.Iterator <T> () {
+        int index = 0;
+        public boolean hasNext() { return index < len; }
+        public T next() { return arr[index+++]; }
+
+    };
+}
+
+@Override public String toString() {
+    if (len == 0) return "[]";
+    else {
+        StringBuilder sb = new StringBuilder( len ).append("[");
+        for (int i = 0; i < len-1; i++ )
+        sb.append(arr[i] + ", ");
+        return sb.append(arr[len-1] + "]").toString();
+    }
+}
