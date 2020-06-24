69a479805fc035a43e2f725cce65c765345ad488
Anya <annagapyuk@gmail.com>
2020-06-24 17:20:51 -0400

    created an empty stack

---
+++ stacks.java
@@ -0,0 +1,6 @@
+public class Stack <T> implements Iterable <T> {
+    private java.util.LinkedList <T> list = new java.util.LinkedList <T>();
+
+    // create an empty stack
+    public Stack() { }
+}
