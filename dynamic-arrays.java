80852609622af2f0ec10564a9bc8ba45b9e1ae78
Anya <annagapyuk@gmail.com>
2020-06-30 23:05:49 -0400

    just edited comments a bit

--- dynamic-arrays.java
+++ dynamic-arrays.java
@@ -1,5 +1,5 @@
 private T [] arr; // internal static array
-private int len = 0; // length user thinks array is
+private int len = 0; // the length user thinks array is
 private int capacity = 0; // actual array size
 
 public Array() { // constructor
@@ -18,7 +18,7 @@ public boolean isEmpty() { return size() == 0; }
 public T get(int index) { return arr[index]; } // bounce check needed
 public void set(int index, T elem) { arr[index] = elem; } // bounce check needed
 
-public void clear() {
+public void clear() { // clear function
     for(int i = 0; i < capacity; i++)
     arr[i] = null;
     len = 0;
@@ -27,6 +27,7 @@ public void clear() {
 public void add(T elem) {
 
     // Time to resize!
+    
     if (len+1 >= capacity) {
         if (capacity == 0) capacity = 1;
         else capacity *= 2; // double the size
